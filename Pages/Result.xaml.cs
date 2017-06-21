6d56e58f50f40a85f397f8e91a13d0ebac80c665
wisteria <wisteria0x0f@users.noreply.github.com>
2017-06-22 01:02:54 +0900

    説明書きを加えるなどした

--- Pages/Result.xaml.cs
+++ Pages/Result.xaml.cs
@@ -34,19 +34,20 @@ namespace WpfApplication1
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            
-            foreach(var i in Common.candidates)
+
+            foreach (var i in Common.candidates)
             {
-                if(results.Any(x => x.Key == i)) continue;
+                if (results.Any(x => x.Key == i)) continue;
                 results.Add(i, 0);
             }
 
             foreach (var d in Common.inputData)
             {
-                if (d.Length == 0) {
+                if (d.Length == 0)
+                {
                     continue;
                 }
-                foreach(var day in d)
+                foreach (var day in d)
                 {
                     //if(!results.Any(x => x.Key == i)) continue;
                     results[day]++;
@@ -56,20 +57,24 @@ namespace WpfApplication1
             var m_dt = new DataTable("DataGridTest");
 
             m_dt.Columns.Add(new DataColumn("date", typeof(string)));
-            m_dt.Columns.Add(new DataColumn("population", typeof(int)));
+            m_dt.Columns.Add(new DataColumn("population", typeof(string)));
 
             // サンプルデータ追加
             DataRow newRowItem;
             foreach (var r in results)
             {
+                if (r.Value == 0) continue;
                 newRowItem = m_dt.NewRow();
                 newRowItem["date"] = r.Key.ToShortDateString();
-                newRowItem["population"] = r.Value;
+                newRowItem["population"] = r.Value + (r.Value == results.Max(x => x.Value) ? " ★" : "");
                 m_dt.Rows.Add(newRowItem);
             }
 
             // グリッドにバインド
             dataGrid.DataContext = m_dt;
+
+            Common.window.Title = "Step3. 日程を決定しましょう。";
+
 
         }
     }
